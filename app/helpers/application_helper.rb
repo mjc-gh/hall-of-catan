afb2313bf44fd5c2a73d342aa95353a9fda0c0c2
mikeycgto <mikeycgto@gmail.com>
2011-12-06 20:37:43 -0500

    add in date time ago helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def date_ago_in_words(date)
+    diff = Date.current <=> date
+
+    if diff.abs > 0
+      "#{distance_of_time_in_words(Date.current, date)} ago"
+    else
+      'today'
+    end
+  end
 end

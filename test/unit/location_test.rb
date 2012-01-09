311b7fe14d46a03d0b8820089b38d2bffd8a6bb4
mikeycgto <mikeycgto@gmail.com>
2012-01-08 23:54:02 -0500

    use short factory girl methods

--- test/unit/location_test.rb
+++ test/unit/location_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
   test "game association" do
-    location = Factory.create(:location)
-    game = Factory.create(:game, :location => location)
+    location = create(:location)
+    game = create(:game, :location => location)
     
     assert_equal location, game.location
     assert_equal 1, location.games.size
